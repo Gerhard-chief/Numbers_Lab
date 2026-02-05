8ad85d805783e729c6697ecbca0d589c3f5fc891
Gerhard-chief <Gerhard8RVA@gmail.com>
2026-02-05 17:14:38 +0300

    feat: add min/max/avg and positive/negative/zero counts

--- main.py
+++ main.py
@@ -23,20 +23,35 @@ def calc_stats(numbers):
         stats['min'] = None
         stats['max'] = None
         stats['avg'] = None
+        stats["negatives_count"] = 0
+        stats["positives_count"] = 0
+        stats["zeros_count"] = 0
         return stats
     total = 0
     min_val = numbers[0]
     max_val = numbers[0]
+    pc = 0
+    nc = 0
+    zc = 0
     for x in numbers:
         total += x
         if x < min_val:
             min_val = x
         if x > max_val:
             max_val = x
+        if x == 0:
+            zc += 1
+        if x < 0:
+            nc += 1
+        if x > 0:
+            pc += 1
     stats["sum"] = total
     stats["count"] = len(numbers)
     stats['avg'] = total/len(numbers)
     stats['min'] = min_val
     stats['max'] = max_val
+    stats["negatives_count"] = nc
+    stats["positives_count"] = pc
+    stats["zeros_count"] = zc
 
     return stats
